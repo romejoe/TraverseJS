4b4757c21e838b1af48a7684acb410b53818822f
Joe Stanton <joe@coveycs.com>
2014-01-10 22:15:03 -0500

    extracted splitting pattern out of functions created SetTraverseObject (this function traverses the object and sets the value)

--- Traverse.js
+++ Traverse.js
@@ -1,5 +1,7 @@
+___TraverseJSPattern = "/";
+
 function TraverseObject(path, object, config){
-	var components = path.split("/");
+	var components = path.split(___TraverseJSPattern);
 	for(var i = 0; i < components.length; ++i){
 		if(components[i].charAt(0) == "!"){
 			object = object[config[components[i].substring(1)]];
@@ -9,4 +11,26 @@ function TraverseObject(path, object, config){
 		}
 	}
 	return object;
+}
+
+function SetTraverseObject(value, path, object, config){
+	var components = path.split(___TraverseJSPattern);
+	var componentCache = [];
+	var _cache = [];
+	for(var i = 0; i < components.length; ++i){
+		_cache.push(object);
+		if(components[i].charAt(0) == "!"){
+			componentCache.push(config[components[i].substring(1)]);
+		}
+		else{
+			componentCache.push(components[i]);
+		}
+		object = object[componentCache[i]];
+		
+	}
+	_cache.push(value);
+	for(var i = _cache.length - 2; i >= 0; --i){
+		_cache[i][componentCache[i]] = _cache[i+1];
+	}
+	return _cache[0];
 }
